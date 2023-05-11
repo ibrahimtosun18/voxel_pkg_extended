cc1766c5ac98c7c350265364d6c36731e8096518
Ibrahim TOSUN <ibrahim.tsn18@gmail.com>
2023-05-11 19:32:28 +0300

    Add files via upload

--- src/mls_upsampler/Mls_upsampler.cpp
+++ src/mls_upsampler/Mls_upsampler.cpp
@@ -2,15 +2,20 @@
 
 namespace mls_upsampling
 {
-    MSLUpsampler::MSLUpsampler(ros::NodeHandle &nh) : nh_(nh)
+    // Constructor
+    MLSUpsampler::MLSUpsampler(ros::NodeHandle &nh) : nh_(nh)
     {
+        // Get the parameters from the launch file
         nh_.param<std::string>("input_pcd_file", m_input_pcd_file, "");
         nh_.param<std::string>("output_pcd_file", m_output_pcd_file, "");
 
+        //sets the default leaf size of the voxel grid filter but can be changed in the launch file
         nh_.param("search_radius", m_search_radius, 0.1);
 
+        // I also added a publisher in case of using the voxel filter as a subscriber from other nodes
         point_publisher = nh_.advertise<sensor_msgs::PointCloud2>("upsampled_cloud", 1);
 
+        //Giving information about the node
         std::cout << "  =============================================================== " << std::endl;
         std::cout << "  MLS UPSAMPLER NODE IS STARTED FOR UP SAMPLING THE POINT CLOUD" << std::endl;
         std::cout << "  =============================================================== " << std::endl;
@@ -24,20 +29,30 @@ namespace mls_upsampling
         std::cout << "  SEARCH RADIUS: " << m_search_radius << std::endl;
         std::cout << "  ************************************************************************" << std::endl;
     }
-
-    void MSLUpsampler::processPointCloud()
+    // Destructor
+    MLSUpsampler::~MLSUpsampler()
     {
+    }
+
+    // This function is used to process the point cloud. It upsamples it and visualize it then publish it
+    void MLSUpsampler::processPointCloud()
+    {
+        // Load the point cloud from the input file
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
+        // check if the input file is empty
         if (pcl::io::loadPCDFile(m_input_pcd_file, *cloud) < 0)
         {
             PCL_ERROR("Failed to load file %s\n", m_input_pcd_file.c_str());
             return;
         }
 
+        // Create the output point cloud
         pcl::PointCloud<pcl::PointXYZ>::Ptr upsampled_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        // Create the search tree
         pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
 
+        // Create the object for the upsampling
         pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointXYZ> mls;
         mls.setComputeNormals(true);
         mls.setInputCloud(cloud);
@@ -45,14 +60,18 @@ namespace mls_upsampling
         mls.setSearchMethod(tree);
         mls.setSearchRadius(m_search_radius);
 
+        // Upsampling the point cloud
         mls.setUpsamplingMethod(pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointXYZ>::SAMPLE_LOCAL_PLANE);
         mls.setUpsamplingRadius(0.025);
         mls.setUpsamplingStepSize(0.01);
-            
+    
+        // Reconstruct
         mls.process(*upsampled_cloud);
 
+        // Save the output point cloud
         pcl::io::savePCDFileASCII(m_output_pcd_file, *upsampled_cloud);
 
+        // Create the viewers
         pcl::visualization::PCLVisualizer::Ptr input_viewer(new pcl::visualization::PCLVisualizer("Input Cloud Viewer"));
         pcl::visualization::PCLVisualizer::Ptr upsampled_viewer(new pcl::visualization::PCLVisualizer("Upsampled Cloud Viewer"));
 
@@ -60,9 +79,6 @@ namespace mls_upsampling
         sensor_msgs::PointCloud2 output;
         pcl::toROSMsg(*upsampled_cloud, output);
         output.header.frame_id = "upsampled_cloud";
-
-        
-
 
         // Set the background color
         input_viewer->setBackgroundColor(0, 0, 0);
