b5674b2b06996a3b71488276b858fe8ccd4f7198
Ibrahim TOSUN <ibrahim.tsn18@gmail.com>
2023-05-11 19:32:46 +0300

    Add files via upload

--- src/mls_upsampler/mls_upsampler_node.cpp
+++ src/mls_upsampler/mls_upsampler_node.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "mls_upsampling_node");
     ros::NodeHandle nh("~");
 
-    mls_upsampling::MSLUpsampler mu(nh);
+    mls_upsampling::MLSUpsampler mu(nh);
     mu.processPointCloud(); // Call the processPointCloud() function
 
     return 0;
